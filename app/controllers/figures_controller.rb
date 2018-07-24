ff579be51826c9715d8fbfaaca7cf0865affad53
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-24 21:45:28 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,10 +5,10 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure = Figure.new(params[:figure])
+
     if !params[:title][:name].empty?
       @figure.titles << Title.new(name: params[:title][:name])
     end
-
     if !params[:landmark][:name].empty?
       @figure.landmarks << Landmark.new(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
     end
@@ -16,4 +16,16 @@ class FiguresController < ApplicationController
     @figure.save
 
     redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures' do
+    erb :'figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/show'
+  end
+
 end
