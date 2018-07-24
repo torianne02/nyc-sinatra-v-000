d7643286be795e54697069ca2af8527ee8a361cd
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-24 22:24:03 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,52 @@
 class LandmarksController < ApplicationController
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
 
+  post '/landmarks' do
+    @landmark = Landmark.new(params[:landmark])
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.new(name: params[:title][:name])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.new(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    end
+
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/landmarks' do
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.new(name: params[:title][:name])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.new(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    end
+
+    @figure.save
+
+    redirect to "/figures/#{@figure.id}"
+  end
 end
