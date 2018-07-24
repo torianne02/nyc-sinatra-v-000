60c96b249f6584201049c461710a7b70df67bc1f
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-24 21:52:34 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -34,4 +34,14 @@ class FiguresController < ApplicationController
     erb :'/figures/edit'
   end
 
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.new(name: params[:title][:name])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.new(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+
 end
