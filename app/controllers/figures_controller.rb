ae6676dd537c9bf13e8b86bfef4ecb8940eedb7f
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-24 21:41:03 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,11 @@
 class FiguresController < ApplicationController
-  get :'/figures/new' do 
+  get :'/figures/new' do
     erb :'figures/new'
   end
+
+  post '/figures' do
+    @figure = Figure.new(params[:figure])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.new(name: params[:title][:name])
+    end
 end
